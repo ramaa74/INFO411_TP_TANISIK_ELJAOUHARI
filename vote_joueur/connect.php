dd627e2999490f85d7c335494857d64bd3ae297f
ramaa74 <152201065+ramaa74@users.noreply.github.com>
2025-03-28 16:09:41 +0100

    Update connect.php

--- vote_joueur/connect.php
+++ vote_joueur/connect.php
@@ -3,4 +3,5 @@ define('SERVEUR_BD', 'localhost'); // Adresse du serveur
 define('LOGIN_BD', 'root');        // Identifiant MySQL (par défaut sous MAMP)
 define('PASS_BD', 'root');         // Mot de passe MySQL (par défaut sous MAMP)
 define('NOM_BD', 'vote_joueur');   // Nom de la base de données
+drgzer
 ?>
